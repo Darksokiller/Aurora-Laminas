5a2f68c76561d8a6be7224a2cb3dc40257fd5681
Tyrsson <jsmith@webinertia.net>
2021-10-15 23:16:30 -0500

    adding a call to the controller plugin helper in album controller as a test

--- module/Album/src/Controller/AlbumController.php
+++ module/Album/src/Controller/AlbumController.php
@@ -3,11 +3,15 @@ namespace Album\Controller;
 
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
+use Laminas\Mvc\Controller\Plugin\Layout;
 
 class AlbumController extends AbstractActionController
 {
     public function indexAction()
     {
+        $layout = new Layout();
+        
+        var_dump($layout);
     }
     
     public function addAction()
